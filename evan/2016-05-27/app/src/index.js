7c4b5865fe8efbb5977270b1d0b039fea419c7b5
Evan Carey <evanacarey@gmail.com>
2016-05-26 09:58:53 -0700

    Added a spinning cube. Fixed resize event listener

--- evan/2016-05-27/app/src/index.js
+++ evan/2016-05-27/app/src/index.js
@@ -1,34 +1,49 @@
 var Renderer = (function () {
     function Renderer(width, height) {
+        var _this = this;
         this.width = width;
         this.height = height;
         this.frame = 0;
+        this.onWindowResize = function () {
+            _this.windowHalfX = window.innerWidth / 2;
+            _this.windowHalfY = window.innerHeight / 2;
+            _this.camera.aspect = window.innerWidth / window.innerHeight;
+            _this.camera.updateProjectionMatrix();
+            _this.renderer.setSize(window.innerWidth, window.innerHeight);
+        };
         this.renderer = new THREE.WebGLRenderer({ alpha: true });
         this.renderer.setSize(width, height);
         this.renderer.setClearColor(0x000000, 1);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
         document.body.appendChild(this.renderer.domElement);
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
-        this.camera.position.set(0, 0, 20);
+        this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+        this.camera.position.set(0, 1, 5);
         this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+        var pLight = new THREE.DirectionalLight(0xffffff, .7);
+        pLight.position.set(10, 10, 0);
+        this.scene.add(pLight);
+        var aLight = new THREE.AmbientLight(0xffffff, 0.1);
+        this.scene.add(aLight);
         window.addEventListener("resize", this.onWindowResize, false);
         document.addEventListener("mousemove", this.onMouseMove, false);
-        var geometry = new THREE.BoxGeometry(1, 1, 1);
-        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        var cube = new THREE.Mesh(geometry, material);
-        this.scene.add(cube);
+        var geometry = new THREE.BoxGeometry(2, 2, 2);
+        var material = new THREE.MeshPhongMaterial({
+            color: 0x00ff00,
+            specular: 0xffffff,
+            shininess: 56
+        });
+        this.cube = new THREE.Mesh(geometry, material);
+        this.scene.add(this.cube);
     }
-    Renderer.prototype.onWindowResize = function () {
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-    };
     Renderer.prototype.onMouseMove = function (event) {
-        event.preventDefault();
+        var mouseX = (event.clientX - this.windowHalfX);
+        var mouseY = (event.clientY - this.windowHalfY);
     };
     Renderer.prototype.update = function () {
         var time = Date.now() * 0.001;
         this.frame += 0.01;
+        this.cube.rotateY(0.01);
     };
     Renderer.prototype.render = function () {
         var _this = this;
--- evan/2016-05-27/app/src/index.ts
+++ evan/2016-05-27/app/src/index.ts
@@ -1,8 +1,12 @@
 class Renderer {
     private renderer: THREE.WebGLRenderer;
     private scene: THREE.Scene;
-    private camera: THREE.PerspectiveCamera;
+    public camera: THREE.PerspectiveCamera;
     private frame: number = 0;
+    public windowHalfX: number;
+    public windowHalfY: number;
+
+    public cube: THREE.Mesh;
 
     constructor(public width: number, public height: number) {
         // create renderer
@@ -11,6 +15,7 @@ class Renderer {
         // set dimensions
         this.renderer.setSize(width, height);
         this.renderer.setClearColor(0x000000, 1);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
 
         // bind renderer to DOM
         document.body.appendChild(this.renderer.domElement);
@@ -19,36 +24,58 @@ class Renderer {
         this.scene = new THREE.Scene();
 
         // create camera
-        this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
+        this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
         // position camera
-        this.camera.position.set(0, 0, 20);
+        this.camera.position.set(0, 1, 5);
         this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+        // add a light
+        let pLight = new THREE.DirectionalLight(0xffffff, .7);
+        pLight.position.set(10, 10, 0);
+        this.scene.add(pLight);
+        let aLight = new THREE.AmbientLight(0xffffff, 0.1);
+        this.scene.add(aLight);
 
         // add some listeners
         window.addEventListener("resize", this.onWindowResize, false);
         document.addEventListener("mousemove", this.onMouseMove, false);
 
-        let geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
-        let material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        let cube: THREE.Mesh = new THREE.Mesh(geometry, material);
-        this.scene.add(cube);
+        // add an object
+        let geometry: THREE.BoxGeometry = new THREE.BoxGeometry(2, 2, 2);
+        let material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial(
+            {
+                color: 0x00ff00,
+                specular: 0xffffff,
+                shininess: 56
+            }
+        );
+        this.cube = new THREE.Mesh(geometry, material);
+        this.scene.add(this.cube);
     }
 
-    onWindowResize() {
+    private onWindowResize = () => {
+        this.windowHalfX = window.innerWidth / 2;
+        this.windowHalfY = window.innerHeight / 2;
+
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
+    // TODO: relegate mouse events to separate class InputHandler
     onMouseMove(event) {
-        event.preventDefault();
+        // event.preventDefault();
+
+        let mouseX = (event.clientX - this.windowHalfX);
+        let mouseY = (event.clientY - this.windowHalfY);
     }
 
 
     update() {
         let time: number = Date.now() * 0.001;
         this.frame += 0.01;
+        this.cube.rotateY(0.01);
     }
 
     render() {
