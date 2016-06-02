2af2932b7de051a990931d84666ad05c3a1f1422
Evan Carey <evanacarey@gmail.com>
2016-06-02 00:27:25 -0700

    Basic camera movement. Janky

--- app/src/main.js
+++ app/src/main.js
@@ -1,15 +1,97 @@
+var MOUSE_SENSITIVITY = 10;
+var DEG_TO_RADS = 3.141592654 / 180.0;
+var Key = {
+    W: 87,
+    S: 83,
+    A: 65,
+    D: 68,
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
+    SPACE: 32
+};
 var Renderer = (function () {
     function Renderer(width, height) {
         var _this = this;
         this.width = width;
         this.height = height;
-        this.frame = 0;
+        this.cMatrix = new THREE.Matrix4();
+        this.camXAngle = 0.0;
+        this.camYAngle = 0.0;
+        this.camXPos = 0.0;
+        this.camYPos = 3.0;
+        this.camZPos = 5.0;
+        this.camXSpeed = 0.0;
+        this.camYSpeed = 0.0;
+        this.camZSpeed = 0.0;
+        this.speed = 0.2;
+        this.moveForward = false;
+        this.moveBackward = false;
+        this.moveRight = false;
+        this.moveLeft = false;
+        this.moveUp = false;
+        this.moveDown = false;
+        this.frames = 0;
         this.onWindowResize = function () {
             _this.windowHalfX = window.innerWidth / 2;
             _this.windowHalfY = window.innerHeight / 2;
             _this.camera.aspect = window.innerWidth / window.innerHeight;
             _this.camera.updateProjectionMatrix();
             _this.renderer.setSize(window.innerWidth, window.innerHeight);
+        };
+        this.onKeyDown = function (event) {
+            switch (event.keyCode) {
+                case Key.UP:
+                case Key.W:
+                    _this.moveForward = true;
+                    break;
+                case Key.DOWN:
+                case Key.S:
+                    _this.moveBackward = true;
+                    break;
+                case Key.LEFT:
+                case Key.A:
+                    _this.moveLeft = true;
+                    break;
+                case Key.RIGHT:
+                case Key.D:
+                    _this.moveRight = true;
+                    break;
+            }
+            var mouseX = (event.clientX - _this.windowHalfX);
+            var mouseY = (event.clientY - _this.windowHalfY);
+            _this.calcCameraMovement();
+            var camXAngle = mouseX / MOUSE_SENSITIVITY;
+            var camYAngle = mouseY / MOUSE_SENSITIVITY;
+            if (camXAngle <= -90.0)
+                camXAngle = -89.9;
+            if (camXAngle >= 90.0)
+                camXAngle = 89.9;
+            if (camYAngle < -180.0)
+                camYAngle += 360.0;
+            if (camYAngle > 180.0)
+                camYAngle += 360.0;
+        };
+        this.onKeyUp = function (event) {
+            switch (event.keyCode) {
+                case Key.UP:
+                case Key.W:
+                    _this.moveForward = false;
+                    break;
+                case Key.DOWN:
+                case Key.S:
+                    _this.moveBackward = false;
+                    break;
+                case Key.LEFT:
+                case Key.A:
+                    _this.moveLeft = false;
+                    break;
+                case Key.RIGHT:
+                case Key.D:
+                    _this.moveRight = false;
+                    break;
+            }
         };
         this.renderer = new THREE.WebGLRenderer({ alpha: true });
         this.renderer.setSize(width, height);
@@ -18,7 +100,7 @@ var Renderer = (function () {
         document.body.appendChild(this.renderer.domElement);
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-        this.camera.position.set(0, 3, 5);
+        this.camera.position.set(this.camXPos, this.camYPos, this.camZPos);
         this.camera.lookAt(new THREE.Vector3(0, 0, 0));
         var pLight = new THREE.PointLight(0xffffff, .7);
         pLight.position.set(3, 10, 3);
@@ -40,16 +122,63 @@ var Renderer = (function () {
         this.cube.translateY(1);
         this.scene.add(this.cube);
         window.addEventListener("resize", this.onWindowResize, false);
-        document.addEventListener("mousemove", this.onMouseMove, false);
+        document.addEventListener("keydown", this.onKeyDown, false);
+        document.addEventListener("keyup", this.onKeyUp, false);
+        this.startTime = Date.now();
     }
-    Renderer.prototype.onMouseMove = function (event) {
-        var mouseX = (event.clientX - this.windowHalfX);
-        var mouseY = (event.clientY - this.windowHalfY);
+    Renderer.prototype.calcCameraMovement = function () {
+        var xMovement = 0.0, yMovement = 0.0, zMovement = 0.0;
+        if (this.moveForward) {
+            var pitch = Math.cos(this.camXAngle * DEG_TO_RADS);
+            xMovement += this.speed * Math.sin(this.camYAngle * DEG_TO_RADS) * pitch;
+            yMovement += this.speed * -Math.sin(this.camYAngle * DEG_TO_RADS);
+            var yaw = Math.cos(this.camXAngle * DEG_TO_RADS);
+            zMovement = this.speed * -Math.cos(this.camYAngle * DEG_TO_RADS) * yaw;
+        }
+        if (this.moveBackward) {
+            var pitch = Math.cos(this.camXAngle * DEG_TO_RADS);
+            xMovement += this.speed * -Math.sin(this.camYAngle * DEG_TO_RADS) * pitch;
+            yMovement += this.speed * Math.sin(this.camYAngle * DEG_TO_RADS);
+            var yaw = Math.cos(this.camXAngle * DEG_TO_RADS);
+            zMovement = this.speed * Math.cos(this.camYAngle * DEG_TO_RADS) * yaw;
+        }
+        if (this.moveLeft) {
+            var camYAngleRad = this.camYAngle * DEG_TO_RADS;
+            xMovement += -this.speed * Math.cos(camYAngleRad);
+            zMovement += -this.speed * Math.sin(camYAngleRad);
+        }
+        if (this.moveRight) {
+            var camYAngleRad = this.camYAngle * DEG_TO_RADS;
+            xMovement += this.speed * Math.cos(camYAngleRad);
+            zMovement += this.speed * Math.sin(camYAngleRad);
+        }
+        if (xMovement > this.speed)
+            xMovement = this.speed;
+        if (xMovement < -this.speed)
+            xMovement = -this.speed;
+        if (yMovement > this.speed)
+            yMovement = this.speed;
+        if (yMovement < -this.speed)
+            yMovement = -this.speed;
+        if (zMovement > this.speed)
+            zMovement = this.speed;
+        if (zMovement < -this.speed)
+            zMovement = -this.speed;
+        this.camXPos += xMovement;
+        this.camYPos += yMovement;
+        this.camZPos += zMovement;
+        this.camera.position.set(this.camXPos, this.camYPos, this.camZPos);
     };
     Renderer.prototype.update = function () {
-        var time = Date.now() * 0.001;
-        this.frame += 0.01;
         this.cube.rotateY(0.01);
+    };
+    Renderer.prototype.timer = function () {
+        var dt = Date.now() - this.startTime;
+        if (dt >= 1000) {
+            this.frames = 0;
+            this.startTime = Date.now();
+        }
+        this.frames++;
     };
     Renderer.prototype.render = function () {
         var _this = this;
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,13 +1,54 @@
+const MOUSE_SENSITIVITY: number = 10;
+const DEG_TO_RADS: number = 3.141592654 / 180.0; // conversion of 1 degree to radians
+
+const Key = {
+    W: 87,
+    S: 83,
+    A: 65,
+    D: 68,
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
+    SPACE: 32
+};
+
+
 class Renderer {
+
     private renderer: THREE.WebGLRenderer;
     private scene: THREE.Scene;
-    public camera: THREE.PerspectiveCamera;
-    private frame: number = 0;
-    public windowHalfX: number;
-    public windowHalfY: number;
+
+
+    private camera: THREE.PerspectiveCamera;
+    private cMatrix: THREE.Matrix4 = new THREE.Matrix4();
+
+    // angle of rotation for the camera
+    private camXAngle: number = 0.0; // relative to x-axis (i.e. vertical angle)
+    private camYAngle: number = 0.0; // relative to y-axis (i.e. horizontal angle)
+
+    // XYZ position of the camera
+    private camXPos: number = 0.0; private camYPos: number = 3.0; private camZPos: number = 5.0;
+
+    // movement speed of camera
+    private camXSpeed: number = 0.0; private camYSpeed: number = 0.0; private camZSpeed: number = 0.0;
+
+    // adjust to change movement speed
+    private speed: number = 0.2;
+
+    // directional movement
+    private moveForward: boolean = false; private moveBackward: boolean = false;
+    private moveRight: boolean = false; private moveLeft: boolean = false;
+    private moveUp: boolean = false; private moveDown: boolean = false;
+
+    public windowHalfX: number; public windowHalfY: number;
 
     public cube: THREE.Mesh;
     public plane: THREE.Mesh;
+
+    /** Timer */
+    private frames: number = 0;
+    private startTime: number;
 
     constructor(public width: number, public height: number) {
         // create renderer
@@ -29,7 +70,7 @@ class Renderer {
         this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
 
         // position camera
-        this.camera.position.set(0, 3, 5);
+        this.camera.position.set(this.camXPos, this.camYPos, this.camZPos);
         this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
         /*
@@ -75,7 +116,11 @@ class Renderer {
 
         // add some listeners
         window.addEventListener("resize", this.onWindowResize, false);
-        document.addEventListener("mousemove", this.onMouseMove, false);
+        document.addEventListener("keydown", this.onKeyDown, false);
+        document.addEventListener("keyup", this.onKeyUp, false);
+
+        // initialize timer
+        this.startTime = Date.now();
     }
 
     private onWindowResize = () => {
@@ -88,18 +133,123 @@ class Renderer {
     };
 
     // TODO: relegate mouse events to separate class InputHandler
-    onMouseMove(event) {
-        // event.preventDefault();
+    private onKeyDown = (event) => {
+
+        switch (event.keyCode) {
+            case Key.UP:
+            case Key.W:
+                this.moveForward = true;
+                break;
+            case Key.DOWN:
+            case Key.S:
+                this.moveBackward = true;
+                break;
+            case Key.LEFT:
+            case Key.A:
+                this.moveLeft = true;
+                break;
+            case Key.RIGHT:
+            case Key.D:
+                this.moveRight = true;
+                break;
+        }
 
         let mouseX = (event.clientX - this.windowHalfX);
         let mouseY = (event.clientY - this.windowHalfY);
+
+        this.calcCameraMovement();
+
+        let camXAngle = mouseX / MOUSE_SENSITIVITY;
+        let camYAngle = mouseY / MOUSE_SENSITIVITY;
+
+        // limit vertical rotation (stap at straight up and straight down)
+        if (camXAngle <= -90.0) camXAngle = -89.9;
+        if (camXAngle >= 90.0) camXAngle = 89.9;
+        // keep horizontal rotation between -180 and 180 (just so values are easier to interpret)
+        if (camYAngle < -180.0) camYAngle += 360.0;
+        if (camYAngle > 180.0) camYAngle += 360.0;
+    }
+
+    private onKeyUp = (event) => {
+        switch (event.keyCode) {
+            case Key.UP:
+            case Key.W:
+                this.moveForward = false;
+                break;
+            case Key.DOWN:
+            case Key.S:
+                this.moveBackward = false;
+                break;
+            case Key.LEFT:
+            case Key.A:
+                this.moveLeft = false;
+                break;
+            case Key.RIGHT:
+            case Key.D:
+                this.moveRight = false;
+                break;
+        }
+    }
+
+    calcCameraMovement() {
+        let xMovement: number = 0.0, yMovement: number = 0.0, zMovement: number = 0.0;
+
+        if (this.moveForward) {
+            let pitch = Math.cos(this.camXAngle * DEG_TO_RADS);
+            xMovement += this.speed * Math.sin(this.camYAngle * DEG_TO_RADS) * pitch;
+            yMovement += this.speed * -Math.sin(this.camYAngle * DEG_TO_RADS);
+            let yaw = Math.cos(this.camXAngle * DEG_TO_RADS);
+            zMovement = this.speed * -Math.cos(this.camYAngle * DEG_TO_RADS) * yaw;
+        }
+        if (this.moveBackward) {
+            let pitch = Math.cos(this.camXAngle * DEG_TO_RADS);
+            xMovement += this.speed * -Math.sin(this.camYAngle * DEG_TO_RADS) * pitch;
+            yMovement += this.speed * Math.sin(this.camYAngle * DEG_TO_RADS);
+            let yaw = Math.cos(this.camXAngle * DEG_TO_RADS);
+            zMovement = this.speed * Math.cos(this.camYAngle * DEG_TO_RADS) * yaw;
+        }
+        if (this.moveLeft) {
+            let camYAngleRad = this.camYAngle * DEG_TO_RADS;
+            xMovement += -this.speed * Math.cos(camYAngleRad);
+            zMovement += -this.speed * Math.sin(camYAngleRad);
+        }
+        if (this.moveRight) {
+            let camYAngleRad = this.camYAngle * DEG_TO_RADS;
+            xMovement += this.speed * Math.cos(camYAngleRad);
+            zMovement += this.speed * Math.sin(camYAngleRad);
+        }
+
+        // limit movement speed (so diagonal doesn't make you go faster)
+        if (xMovement > this.speed) xMovement = this.speed;
+        if (xMovement < -this.speed) xMovement = -this.speed;
+        if (yMovement > this.speed) yMovement = this.speed;
+        if (yMovement < -this.speed) yMovement = -this.speed;
+        if (zMovement > this.speed) zMovement = this.speed;
+        if (zMovement < -this.speed) zMovement = -this.speed;
+
+
+        // update camera position
+        this.camXPos += xMovement;
+        this.camYPos += yMovement;
+        this.camZPos += zMovement;
+
+        this.camera.position.set(this.camXPos, this.camYPos, this.camZPos);
     }
 
 
     update() {
-        let time: number = Date.now() * 0.001;
-        this.frame += 0.01;
+        // this.timer();
         this.cube.rotateY(0.01);
+    }
+
+    timer() {
+        let dt: number = Date.now() - this.startTime;
+        if (dt >= 1000) {
+            // console.log("FPS: " + this.frames);
+            this.frames = 0;
+            this.startTime = Date.now();
+        }
+        this.frames++;
     }
 
     render() {
