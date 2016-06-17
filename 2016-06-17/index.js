a613c05ce80e8b049621f56ed4b2e8491d078048
Carson Drake <carson@greekrush.com>
2016-06-17 07:50:52 -0700

    What, so everyones supposed to sleep every single night now? You realize that nighttime makes up half of all time?

---
+++ 2016-06-17/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+
+var Agenda = require('agenda');
+var Agendash = require('agendash');
+
+var mongoConnectionString = require('./config');
+
+var agenda = new Agenda({db: {address: mongoConnectionString},processEvery: '30 seconds'});
+var worker = require('./app/agenda.js');
+//
+//
+
+//
+//
+// agenda.on('complete', function(job) {
+// 	console.log("Job %s finished", job.attrs.name);
+// });
+
+
+
+
+app.use('/agendash', Agendash(agenda));
+
+app.get('/test', function (req, res) {
+	worker.now('test');
+	res.send('Hello from A!');
+});
+app.get('/carson', function (req, res) {
+	worker.now('carson');
+	res.send('Hello carson!');
+});
+app.get('/rick', function (req, res) {
+	worker.now('rick');
+	res.send('Hello rick');
+});
+app.get('/evan', function (req, res) {
+	worker.now('evan');
+	res.send('Hello evan!');
+});
+app.get('/cody', function (req, res) {
+	worker.now('cody');
+	res.send('Oh hey Cody...!');
+});
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
+});
